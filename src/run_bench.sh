b0e485074b34884c3839fbd2c0a0b7823e41a871
Anand J <anandj@cs.toronto.edu>
2020-12-16 16:05:42 -0500

    Update run_bench.sh usage

--- src/run_bench.sh
+++ src/run_bench.sh
@@ -7,6 +7,6 @@ if [ "$ENGINE" == "lifestream" ] || [ "$ENGINE" == "trill" ]; then
 elif [ "$ENGINE" == "numlib" ]; then
     python3 numlib.py $DURATION $BENCH
 else
-    echo "Usage: ./run.sh <duration (sec)> (trill|numlib|lifestream) [(normalize|passfilter|fillconst|fillmean|resample|endtoend)]"
+    echo "Usage: ./run.sh <data size (sec)> (trill|numlib|lifestream) [(normalize|passfilter|fillconst|fillmean|resample|endtoend)]"
     exit 1
 fi
